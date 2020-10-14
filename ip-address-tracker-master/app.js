9542c125eaeb9e4bc184a7fea4337da9f63a7e70
Mitch Brunke <mbrunke@fcte.com.au>
2020-10-14 16:54:57 +1000

    api in progress

--- ip-address-tracker-master/app.js
+++ ip-address-tracker-master/app.js
@@ -1,7 +1,23 @@
 console.log("hello there");
+const key = "at_0O4oUtIPLre3fboFvRVbHdhoWX0mS";
+const endpoint =
+  "https://geo.ipify.org/api/v1?apiKey=at_0O4oUtIPLre3fboFvRVbHdhoWX0mS&ipAddress=8.8.8.8";
 
-//form - prevent default
-
+// // ?apiKey=at_0O4oUtIPLre3fboFvRVbHdhoWX0mS&ipAddress=103.74.67.28
 //api - fetch & update dom
+fetch(endpoint, {
+  method: "GET",
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.log("Error"));
 
-//maps - update map based on the input
+// //use form input to update url and dom
+
+// //dom manipulation
+
+// //maps - update map based on the input
+
+// const ipKey = "at_0O4oUtIPLre3fboFvRVbHdhoWX0mS";
