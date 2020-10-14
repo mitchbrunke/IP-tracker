b0041dca72839dc5259866e31d56d9421fb0c72b
Mitch Brunke <mbrunke@fcte.com.au>
2020-10-15 09:41:35 +1000

    fetch works

--- ip-address-tracker-master/app.js
+++ ip-address-tracker-master/app.js
@@ -2,6 +2,8 @@ console.log("hello there");
 const key = "at_0O4oUtIPLre3fboFvRVbHdhoWX0mS";
 const endpoint =
   "https://geo.ipify.org/api/v1?apiKey=at_0O4oUtIPLre3fboFvRVbHdhoWX0mS&ipAddress=8.8.8.8";
+const userInput = document.getElementById("search");
+const submitBtn = document.querySelector("button");
 
 // // ?apiKey=at_0O4oUtIPLre3fboFvRVbHdhoWX0mS&ipAddress=103.74.67.28
 //api - fetch & update dom
@@ -11,7 +13,10 @@ fetch(endpoint, {
   .then((response) => {
     return response.json();
   })
-  .then((data) => console.log(data))
+  .then((data) => {
+    let info = data;
+    console.log(info.location.country);
+  })
   .catch((err) => console.log("Error"));
 
 // //use form input to update url and dom
@@ -21,3 +26,8 @@ fetch(endpoint, {
 // //maps - update map based on the input
 
 // const ipKey = "at_0O4oUtIPLre3fboFvRVbHdhoWX0mS";
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(userInput.value);
+});
